f0da832d37c0469b2b419f64fc2d0dc0df74fbb3
Giiorgia <giiorgiana.opreaa98@gmail.com>
2024-11-29 16:45:45 +0200

    changed footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,23 @@
 // src/Components/Footer.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-  return (
+  return(
     <footer className="App_footer">
-      <p>&copy; {new Date().getFullYear()} Your Fitness App. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Fitness App. All rights reserved.</p>
+      <div className="social_icons">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        </div>
     </footer>
   );
 };
